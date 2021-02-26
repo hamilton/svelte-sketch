3e87008c1c9bf7f2971553d3c87135f132a30662
hamilton <hamilton.ulmer@gmail.com>
2021-02-25 17:35:52 -0800

    attempting to create build version

--- index.mjs
+++ index.mjs
@@ -6,15 +6,48 @@ import path from "path";
 import fs from "fs";
 import snowpack from 'snowpack';
 import rimraf from "rimraf";
-import { mainJS, indexHTML, globalCSS } from "./assets.mjs";
+import { mainJS, indexHTML, globalCSS, favicon } from "./assets.mjs";
 
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const { createConfiguration, startServer } = snowpack;
+const { createConfiguration, startServer, build } = snowpack;
 
-const args = yargs(helpers.hideBin(process.argv));
-// gather any css files in args.argv
+const args = yargs(helpers.hideBin(process.argv))
+    .command('watch <component>', 'watch component and serve on 8080', {
+        component: {
+            type: 'string'
+        }
+    })
+    .command('build <component>', 'build final component', {
+        component: {
+            type: 'string'
+        }
+    })
+    .example('$0 watch ./C.svelte', 'watch ExampleComponent.svelte')
+    .example('$0 watch ./C.svelte --css style.css', 'add local style.css & watch')
+    .wrap(90)
+    .array("css")
+    .option("css <stylesheets>", {type: "array", describe: "path(s) to global stylesheets to append to entrypoint"})
+    .help();
+
+const ENTRY = args.argv.component;
+const mode = args.argv._[0];
+
+if (mode === 'bundle') {
+    console.error("bundle not implemented yet.");
+    process.exit();
+}
+
+if (mode === 'watch' && mode === 'compile') {
+    console.error("must choose only one: watch or compile");
+    process.exit();
+}
+
+if (!ENTRY) {
+    console.error("the entrypoint must be listed");
+    process.exit();
+}
 
 function getDirectory(file) {
     return file.split('/').slice(0, -1).join('/');
@@ -35,30 +68,10 @@ function createFile(path, content) {
     fs.writeFileSync(path, content);
 }
 
-const ENTRY = "../gtest/App.svelte";
-
 const fullPath = path.resolve(ENTRY);
 const componentName = getFilename(ENTRY);
 const rootDir = getDirectory(fullPath);
-
 const siteDir = `${__dirname}/_site`;
-
-makeDir(siteDir);
-makeDir(`${siteDir}/bundle`);
-// get css files and put them into site.
-const cssFile = args.argv.css;
-if (cssFile) {
-    const fileContents = fs.readFileSync(path.resolve(cssFile)).toString();
-    createFile(`${siteDir}/${getFilename(cssFile)}`, fileContents);
-} else {
-    createFile(`${siteDir}/global.css`, globalCSS);
-}
-createFile(`${siteDir}/index.html`, indexHTML({
-    css: cssFile ? getFilename(cssFile) : undefined,
-    title: componentName
-}));
-
-createFile(`${rootDir}/_entry.js`, mainJS(ENTRY));
 
 function createMount() {
     // get all css files and place them in site.
@@ -69,25 +82,63 @@ function createMount() {
     return mountPoint;
 }
 
-const config = createConfiguration({
-    mount: createMount(),
-    exclude: ['**/node_modules/**/*', "rollup.config.js", 'sp.js'],
-    plugins: [
-        '@snowpack/plugin-svelte'
-    ],
-    packageOptions: {
-        "source": "remote"
-    },
-    root: __dirname
-});
+// make the entry one way for both watch and compile modes.
+createFile(`${rootDir}/_entry.js`, mainJS(ENTRY));
 
-const server = await startServer({config});
-server.loadUrl('/dist/_entry.js');
+if (mode === 'watch' || mode === 'build') {
+    makeDir(siteDir);
+    makeDir(`${siteDir}/bundle`);
+    // get css files and put them into site.
+    let cssFile;
+    if (args.argv.css) {
+        [cssFile] = args.argv.css;
+    }
 
-function bye() {
+    if (cssFile) {
+        const fileContents = fs.readFileSync(path.resolve(cssFile)).toString();
+        createFile(`${siteDir}/${getFilename(cssFile)}`, fileContents);
+    } else {
+        createFile(`${siteDir}/global.css`, globalCSS);
+    }
+    createFile(`${siteDir}/index.html`, indexHTML({
+        css: cssFile ? getFilename(cssFile) : undefined,
+        title: componentName
+    }));
+
+    createFile(`${siteDir}/favicon.svg`, favicon);
+
+
+    const config = createConfiguration({
+        mount: createMount(),
+        exclude: ['**/node_modules/**/*', "rollup.config.js"],
+        plugins: [
+            '@snowpack/plugin-svelte'
+        ],
+        packageOptions: {
+            "source": "remote",
+            // cache: `${__dirname}/.snowpack`,
+            
+        },
+        buildOptions: {
+            metaUrlPath: `${__dirname}/.snowpack`
+        },
+        root: __dirname,
+    });
+    if (mode === 'watch') {
+        const server = await startServer({config});
+        await server.loadUrl('/dist/_entry.js');
+    } else if (mode === 'build') {
+        const server = await startServer({config});
+        await server.loadUrl('/dist/_entry.js');
+        const {result} = await build({config});
+        console.log(result);
+    }
+}
+
+function byebye() {
     rimraf.sync("_site");
     fs.unlinkSync(`${rootDir}/_entry.js`);
     process.exit();
 }
 
-process.on("SIGINT", bye);
+process.on("SIGINT", byebye);
